bc822a64116dd08ec19eb51ea824ddc2f46cdfdb
prog-nori <norinorimcz@gmail.com>
2021-10-03 22:17:30 +0900

    リストに追加する箇所の実装2

--- out/py2js/src/lib/predefinedVars.py
+++ out/py2js/src/lib/predefinedVars.py
@@ -10,9 +10,21 @@ def func():
 """
 class PredefinedVars(object):
     def __init__(self):
-        self.list_ = list() # リストとして変数一覧を記憶する
+        self.instances = list()
+        self.vars = list() # リストとして変数一覧を記憶する
         self.dict_ = dict() # 辞書として変数一覧を記憶する
         return
+    
+    def add(self, var):
+        if 'self.' in var:
+            # インスタンス変数と判定
+            if not var in self.instances:
+                self.instances.append(var)
+        else:
+            # ローカル変数と判定
+            if not var in self.vars:
+                self.vars.append(var)
+                # self.
     
     # def add(self, path):
     #     """
